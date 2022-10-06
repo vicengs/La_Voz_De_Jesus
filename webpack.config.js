e7afcaa9c0926f62307a5bbb7ce88c6372f957ab
Vicente Garcia <vicen1@hotmail.com>
2022-10-06 14:27:37 -0500

    Set Up

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const config = {
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [{
+                loader: "file-loader",
+                options: {
+                    esModule: false,
+                    name (file) {
+                        return "[path][name].[ext]"
+                    },
+                    publicPath: function(url) {
+                        return url.replace("../", "/assets/")
+                    }
+                }
+            },
+            {
+                loader: 'image-webpack-loader'
+            }]
+        }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        }),
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            start_url: "../index.html",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+              src: path.resolve("assets/img/icons/icon-512x512.png"),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join("assets", "icons")
+            }]
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './'),
+        },
+        compress: true,
+        port: 8080,
+    },
+    mode: 'development'
+};
+module.exports = config;
